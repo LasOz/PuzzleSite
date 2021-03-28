a12ea86a337ae68a5d6b22fcbf238965c34fa1a4
LasOz <laser.beam.ozdonmez@gmail.com>
2021-03-28 16:05:30 +0100

    Start screen

--- server.py
+++ server.py
@@ -1,22 +1,27 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+#from http.server import BaseHTTPRequestHandler, HTTPServer
+import http.server
 import time
 
 hostName = "localhost"
 serverPort = 80
 
-class MyServer(BaseHTTPRequestHandler):
+class MyServer(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        #self.send_response(200)
+        #self.send_header("Content-type", "text/html")
+        #self.end_headers()
+        if self.path == '/':
+            self.path = 'index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+        # else:
+        #     self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        #     self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        #     self.wfile.write(bytes("<body>", "utf-8"))
+        #     self.wfile.write(bytes("<p>This is an example web server. AAA</p>", "utf-8"))
+        #     self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
+    webServer = http.server.HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
