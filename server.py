6fda8dfb2823a594215c164ef689048872552b0b
LasOz <laser.beam.ozdonmez@gmail.com>
2021-03-29 23:25:43 +0100

    Server fixes

--- server.py
+++ server.py
@@ -1,24 +1,28 @@
 #from http.server import BaseHTTPRequestHandler, HTTPServer
 import http.server
 import time
+import mimetypes
+import os
 
 hostName = "localhost"
 serverPort = 80
 
 class MyServer(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        #self.send_response(200)
-        #self.send_header("Content-type", "text/html")
-        #self.end_headers()
-        if self.path == '/':
-            self.path = 'index.html'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
-        # else:
-        #     self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        #     self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        #     self.wfile.write(bytes("<body>", "utf-8"))
-        #     self.wfile.write(bytes("<p>This is an example web server. AAA</p>", "utf-8"))
-        #     self.wfile.write(bytes("</body></html>", "utf-8"))
+        if (self.path == "/"):
+            self.path = "index.html"
+        try:
+            filepath = os.path.join('.', self.path.strip('/'))
+            f = open(filepath)
+        except IOError:
+            self.send_error(404,'File Not Found: %s ' % filepath)
+        else:
+            self.send_response(200)
+            mimetype, _ = mimetypes.guess_type(filepath)
+            self.send_header('Content-type', mimetype)
+            self.end_headers()
+            for s in f:
+                self.wfile.write(s.encode("utf-8"))
 
 if __name__ == "__main__":        
     webServer = http.server.HTTPServer((hostName, serverPort), MyServer)
